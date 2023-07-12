a7bf0ccc24cbb75ef8f80ad8dfbd379d5c81366c
Nader Abusleme <naderabusleme@MacBook-Air-Nader.local>
2023-07-11 23:18:14 -0400

    home, navbar and footer added

---
+++ src/components/Footer.vue
@@ -0,0 +1,65 @@
+<template>
+    <v-footer
+      dark
+      padless
+    >
+      <v-card
+        flat
+        tile
+        class="green lighten-1 white--text text-center"
+      >
+        <v-card-text>
+          <v-btn
+            v-for="icon in icons"
+            :key="icon"
+            class="mx-4 white--text"
+            icon
+          >
+            <v-icon size="24px">
+              {{ icon }}
+            </v-icon>
+          </v-btn>
+        </v-card-text>
+  
+        <v-card-text class="white--text pt-0">
+          Queremos darte la bienvenida a nuestro sitio, donde encontrarás todo lo que buscas en artículos electrónicos, joyería, ropa de hombre y ropa de mujer. Nuestros productos provienen de tiendas de todas partes del mundo y nos hemos preocupado por cumplir los máximos estándares de calidad. Si tienes alguna pregunta o no puedes encontrar ese producto que tanto necesitas, no dudes en contactarte con nuestro equipo de soporte. 
+        </v-card-text>
+  
+        <v-divider></v-divider>
+  
+        <v-card-text class="white--text">
+          {{ new Date().getFullYear() }} — <strong>Vuetify</strong>
+        </v-card-text>
+      </v-card>
+    </v-footer>
+  
+  </template>
+  
+  <script>
+  export default {
+      name: 'footer-vue',
+      // props: {},
+      data: function(){
+          return {
+              icons: [
+          'mdi-facebook',
+          'mdi-twitter',
+          'mdi-linkedin',
+          'mdi-instagram',
+        ],
+          }
+      },
+      // computed: {},
+      //methods: {}
+      // watch: {},
+      // components: {},
+      // mixins: [],
+      // filters: {},
+      // -- Lifecycle Methods
+      // -- End Lifecycle Methods
+  }
+  </script>
+  
+  <style scoped>
+      
+  </style>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,62 @@
+<template>
+<div>
+    <v-app-bar
+      color="deep-purple"
+      dark
+    >
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title class="font-weight-bold">Vinos Comercio</v-toolbar-title>
+      <v-spacer></v-spacer>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      v-model="drawer"
+      absolute
+      temporary
+    >
+      <v-list
+        nav
+        dense
+      >
+        <v-list-item-group
+          v-model="group"
+          active-class="deep-purple--text text--accent-4"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-home</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Home</v-list-item-title>
+          </v-list-item>
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+</div>
+</template>
+
+<script>
+export default {
+    name: 'nav-bar',
+    // props: {},
+    data: function(){
+        return {
+            drawer: false,
+            group: null,
+        }
+    },
+    // computed: {},
+    //methods: {}
+    // watch: {},
+    // components: {},
+    // mixins: [],
+    // filters: {},
+    // -- Lifecycle Methods
+    // -- End Lifecycle Methods
+}
+</script>
+
+<style scoped>
+    
+</style>
